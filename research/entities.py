1fae9b3698bda01bd81f46560ba5d65af90c2feb
hdavis <hdavis@ebay.com>
2016-01-21 18:22:48 -0500

    updated entities

--- research/entities.py
+++ research/entities.py
@@ -1,42 +1,174 @@
-import nltk
-import string
 import operator
+from collections import defaultdict
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
 import timefreq
 
-""" Identify people, places, and hashtags - eventually incoporate time? """
+abbrev_to_state = {
+    "AL": "Alabama",
+    "AK": "Alaska",
+    "AZ": "Arizona",
+    "AR": "Arkansas",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "DC": "Washington D.C.",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "IA": "Iowa",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "ME": "Maine",
+    "MD": "Maryland",
+    "MA": "Massachusetts",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MS": "Mississippi",
+    "MO": "Missouri",
+    "MT": "Montana",
+    "NE": "Nebraska",
+    "NV": "Nevada",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NY": "New York",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "PR": "Puerto Rico",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VT": "Vermont",
+    "VI": "Virgin Islands",
+    "VA": "Virginia",
+    "WA": "Washington",
+    "WV": "West Virginia",
+    "WI": "Wisconsin",
+    "WY": "Wyoming"
+}
 
+state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Puerto Rico": "PR",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virgin Islands": "VI",
+    "Virginia": "VA",
+    "Washington D.C.": "DC",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY"
+}
 
-def format_special(word, symbol):
-    return symbol + word[1:].strip().translate(string.maketrans("", ""), string.punctuation)
-
+custom_stopwords = ['co', 't', 'http', 'https', 'amp', 'rt', 'tco']
 all_tweets = timefreq.load_tweets_by_time()
 
+tokenizer = RegexpTokenizer(r'[\w#@\']+')
 for candidate, tweets in all_tweets.iteritems():
     tweets = timefreq.get_tweets_by_time(all_tweets, candidate, 2016, 1)
-    hashtags = []
-    users = []
+    hashtags = defaultdict(int)
+    users = defaultdict(int)
+    states = defaultdict(int)
+    entities = {}
+    tf = defaultdict(float)
+    df = defaultdict(float)
+    n = 0.0
     for tweet in tweets:
-        new_hashtags = [format_special(word, '#') for word in tweet.split() if word.startswith('#')]
-        new_users = [format_special(word, '@') for word in tweet.split() if word.startswith('@') and word[1:].lower() != candidate]
-        hashtags.extend(new_hashtags)
-        users.extend(new_users)
+        n += 1
+        tweet = tweet.decode('utf-8').strip().lower()
+        seen = set([])
+        sentences = nltk.sent_tokenize(tweet)
+        for sentence in sentences:
+            tokens = tokenizer.tokenize(sentence)
+            for token in tokens:
+                if token in stopwords.words('english') or token in custom_stopwords:
+                    continue
+                if token.startswith('@'):
+                    users[token.replace("'s", "")] += 1
+                if token.startswith('#'):
+                    hashtags[token] += 1
+                if token.upper() in abbrev_to_state or token.title() in state_to_abbrev:
+                    if token.title() in state_to_abbrev:
+                        name = token.title()
+                    else:
+                        name = abbrev_to_state[token.upper()].title()
+                    states[name] += 1
+                if token not in seen:
+                    seen.add(token)
+                    df[token] += 1
+                tf[token] += 1
 
-    top_hashtags = {}
-    top_users = {}
-
-    for hashtag in hashtags:
-        if hashtag not in top_hashtags: top_hashtags[hashtag] = 0
-        top_hashtags[hashtag] += 1
-
-    for user in users:
-        if user not in top_users: top_users[user] = 0
-        top_users[user] += 1
-
+    print "\tFound %s tweets..." % n
+    print "\tTokens:"
+    for t, score in sorted(tf.items(), key=lambda x: -x[1])[:10]:
+        print "\t\t* %s [%s]" % (t, score)
     print "\tHashtags:"
-    for ht, cnt in sorted(top_hashtags.items(), key=operator.itemgetter(1), reverse=True)[:10]:
+    for ht, cnt in sorted(hashtags.items(), key=operator.itemgetter(1), reverse=True)[:10]:
         print "\t\t* %s [%s]" % (ht, cnt)
     print "\tUsers:"
-    for usr, cnt in sorted(top_users.items(), key=operator.itemgetter(1), reverse=True)[:10]:
+    for usr, cnt in sorted(users.items(), key=operator.itemgetter(1), reverse=True)[:10]:
         print "\t\t* %s [%s]" % (usr, cnt)
+    print "\tStates:"
+    for st, cnt in sorted(states.items(), key=operator.itemgetter(1), reverse=True)[:10]:
+        print "\t\t* %s [%s]" % (st, cnt)
 
 
